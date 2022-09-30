93da2f7551ba905fd8d07f0a83ac2ea47d50ffcc
user <tjdgn1125@gmail.com>
2022-09-30 10:59:36 +0900

    v0.1 erp_Proj open

---
+++ src/test/java/com/choongang/eprproject/EprProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choongang.eprproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
