1521af1ba472899f9799a9a642b0a1886ed2d687
user <tjdgn1125@gmail.com>
2022-10-06 11:11:25 +0900

    v0.6.2.4 csh, login 적용

---
+++ src/main/java/com/choongang/eprproject/login/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package com.choongang.eprproject.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String emp_id;
+    private String pw;
+}
