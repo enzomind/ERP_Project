7d9a09f7f791f64f1b56c2eff9d2682c0d6678eb
October7th <qkrdpwls1007@gachon.ac.kr>
2022-10-11 17:55:03 +0900

    v0.7.2.2 yj 업데이트

--- src/main/java/com/choongang/erpproject/edms/dto/Hr_ReportDto.java
+++ src/main/java/com/choongang/erpproject/edms/dto/Hr_ReportDto.java
@@ -1,4 +1,26 @@
 package com.choongang.erpproject.edms.dto;
 
+import lombok.*;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hr_ReportDto {
+    private Long lev_num;
+    private String appr;
+    private String appr_yn;
+    private Date appr_date;
+    private String lev_date;
+    private String lev_res;
+    private Date start_date;
+    private Date end_date;
+    private int lev_count;
+    private String rej_res;
+
+    private String emp_id;
+
 }
