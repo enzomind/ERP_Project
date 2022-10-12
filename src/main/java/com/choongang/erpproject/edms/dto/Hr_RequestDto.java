016ba8f75fb23fa8aab5f7a5efaa93e96fa8d21e
October7th <qkrdpwls1007@gachon.ac.kr>
2022-10-12 15:33:14 +0900

    v0.7.3.0 yj 업데이트

---
+++ src/main/java/com/choongang/erpproject/edms/dto/Hr_RequestDto.java
@@ -0,0 +1,22 @@
+package com.choongang.erpproject.edms.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+//목록 조회 dto
+
+@Data
+public class Hr_RequestDto {
+    private Long levId;
+    private String levNum;
+    private String appr;
+    private LocalDate applDate;
+    private String levType;
+    private String levTitle;
+    private String levContent;
+    private Date startDate;
+    private Date endDate;
+    private Date levTerm;
+}
---
+++ src/main/java/com/choongang/erpproject/edms/dto/Hr_ResponseDto.java
@@ -0,0 +1,24 @@
+package com.choongang.erpproject.edms.dto;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+//상세 조회 dto
+@Getter
+public class Hr_ResponseDto {
+    private Long levId;
+    private String levNum;
+    private String appr;
+    private String apprState;
+    private LocalDate apprDate;
+    private LocalDate applDate;
+    private String levTitle;
+    private String levContent;
+    private Date startdate;
+    private Date endDate;
+    private int levTerm;
+    private String empId;
+
+}
