e65059dd4a65736ffa7112dcc736394b14602fb6
seonghuchoi <tjdgn1125@gmail.com>
2022-11-10 16:58:02 +0900

    v0.9.7.1 sh 패치 급여등록 콤마 정규화 수정

--- src/main/resources/static/js/emp/empJsBox.js
+++ src/main/resources/static/js/emp/empJsBox.js
@@ -8,7 +8,8 @@ let attrResDate = (attrYear + '-' + attrMonth + '-' + attrDate);
 
 
 //이미지 가져오기위한 프로젝트 경로 상황맞춰서 수정 필요
-let myPrjPath = "/Users/seonghuchoi/Downloads/erp_Project/";
+let myPrjPath = "/Users/seonghuchoi/Downloads/intelliJFolder/erp_Project/";
+// let myPrjPath = "C:\\Users\\user\\erp_Project\\";
 
 $(document).ready(function () {
 
@@ -17,18 +18,18 @@ $(document).ready(function () {
         //empId 가져가기
         let hiddenEmpId2 = $(this).parent().siblings('.hiddenEmpId').text();
         //직원조회 테이블
-        $.getScript("/js.emp/empTable.js", function () {
+        $.getScript("/js/emp/empTable.js", function () {
             empGetTable(hiddenEmpId2);
         });
         //직원수정 실행 시 empId 가져가기
-        $.getScript("/js.emp/empUpdate.js", function () {
+        $.getScript("/js/emp/empUpdate.js", function () {
             empUpdateModal(hiddenEmpId2)
         });
     });
 
     //등록 클릭 시 직원등록 모달 팝업
     $('.hiddenEmpInput').click(function () {
-        $.getScript("/js.emp/empInput.js", function () {
+        $.getScript("/js/emp/empInput.js", function () {
             empInputModal();
         });
     });
@@ -59,11 +60,11 @@ $(document).ready(function () {
                         //empId 가져가기
                         let hiddenEmpId2 = $(this).parent().siblings('.hiddenEmpId').text();
                         //직원조회 테이블
-                        $.getScript("/js.emp/empTable.js", function () {
+                        $.getScript("/js/emp/empTable.js", function () {
                             empGetTable(hiddenEmpId2);
                         });
                         //직원수정 실행 시 empId 가져가기
-                        $.getScript("/js.emp/empUpdate.js", function () {
+                        $.getScript("/js/emp/empUpdate.js", function () {
                             empUpdateModal(hiddenEmpId2)
                         });
                     });
